76396ee5615edfbec3422bce3ca57a401223f677
tuan22mse23087 <tuan22mse23087@fsb.edu.vn>
2023-09-19 19:02:33 +0700

    second commit

--- demo.py
+++ demo.py
@@ -1,1 +1,9 @@
-print("hello my neighbor")
+print("hello")
+equation ='x1 +2 * x2 +x3'
+
+def modify_value(x1,x2,x3):
+    result=eval(equation)
+    print(result)
+    return result
+a=5
+modify_value(a,2,3)
